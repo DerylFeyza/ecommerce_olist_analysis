bced906686b270c55883a6102214d16e692ca5a7
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-02-03 00:08:20 +0700

    revise

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,13 +13,28 @@ sns.set_theme(style="dark")
 combined_df = pd.read_csv("./data/combined_df.csv")
 geolocation_df = pd.read_csv("./data/geolocation_dataset.csv")
 
+min_date = combined_df["order_approved_at"].min()
+max_date = combined_df["order_approved_at"].max()
+
+with st.sidebar:
+    start_date, end_date = st.date_input(
+        label="Rentang Waktu",
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date],
+    )
+
+main_df = combined_df[
+    (combined_df["order_approved_at"] >= str(start_date))
+    & (combined_df["order_approved_at"] <= str(end_date))
+]
 
 st.title("Proyek Analisis Data: E-Commerce Public Dataset by Olist")
 st.markdown(
     """
 **Pertanyaan Bisnis:** \n
-1. Bagaimana pengaruh harga produk dengan volume penjualan produk pada tahun 2017?
-2. Wilayah mana yang melakukan pembelian terbanyak dalam tahun 2017?
+1. Bagaimana pengaruh harga produk dengan volume penjualan produk
+2. Wilayah mana yang melakukan pembelian terbanyak
 """
 )
 st.subheader("Deskripsi Data")
@@ -28,8 +43,7 @@ st.subheader("Dataframe")
 st.dataframe(combined_df.head())
 
 
-st.subheader("1. Pengaruh Harga Produk dengan Volume Penjualan Produk pada Tahun 2017")
-combined_2017_df = combined_df[combined_df["year"] == 2017]
+st.subheader("1. Pengaruh Harga Produk dengan Volume Penjualan Produk")
 
 bins = [0, 50, 100, 200, 500, 1000, 5000, 10000, 50000]
 labels = [
@@ -42,19 +56,17 @@ labels = [
     "5000-10000",
     "10000+",
 ]
-combined_2017_df.loc[:, "price_bin"] = pd.cut(
-    combined_2017_df["price"], bins=bins, labels=labels, right=False
+main_df.loc[:, "price_bin"] = pd.cut(
+    main_df["price"], bins=bins, labels=labels, right=False
 )
-combined_2017_sales_df = (
-    combined_2017_df.groupby("price_bin", observed=False)
+main_df = (
+    main_df.groupby("price_bin", observed=False)
     .agg({"product_id": "nunique", "order_item_id": "count", "price": "mean"})
     .reset_index()
 )
 
 plt.figure(figsize=(12, 6))
-sns.barplot(
-    x="price_bin", y="order_item_id", data=combined_2017_sales_df, palette="Blues_r"
-)
+sns.barplot(x="price_bin", y="order_item_id", data=main_df, palette="Blues_r")
 plt.title("Volume Penjualan Berdasarkan Rentang Harga (BRL)", fontsize=14)
 plt.xlabel("Rentang Harga (BRL)", fontsize=12)
 plt.ylabel("Jumlah Produk Terjual", fontsize=12)
@@ -62,18 +74,18 @@ plt.xticks(rotation=45)
 st.pyplot(plt.gcf())
 
 plt.figure(figsize=(6, 4))
-corr_matrix = combined_2017_sales_df[["order_item_id", "price"]].corr()
+corr_matrix = main_df[["order_item_id", "price"]].corr()
 sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
 plt.title("Heatmap Korelasi antara Harga barang dan jumlah terjualnya barang")
 st.pyplot(plt.gcf())
 
 st.markdown(
     """**Pengaruh Harga produk dengan volume pembelian produk**  
-terlihat bahwa terdapat hubungan negatif moderat antara harga produk dan volume penjualan produk. Korelasi antara harga dan volume penjualan adalah -0.58, yang menunjukkan bahwa peningkatan harga cenderung mengurangi volume penjualan produk pada tahun 2017
+terlihat bahwa terdapat hubungan negatif moderat antara harga produk dan volume penjualan produk. Korelasi antara harga dan volume penjualan adalah -0.58, yang menunjukkan bahwa peningkatan harga cenderung mengurangi volume penjualan produk 
 """
 )
 
-st.subheader("2. Wilayah mana yang melakukan pembelian terbanyak dalam tahun 2017?")
+st.subheader("2. Wilayah mana yang melakukan pembelian terbanyak")
 
 r = requests.get("https://servicodados.ibge.gov.br/api/v1/localidades/mesorregioes")
 content = [c["UF"] for c in json.loads(r.text)]
@@ -86,11 +98,9 @@ geo_group = geolocation_df.groupby(
     by="geolocation_zip_code_prefix", as_index=False
 ).min()
 
-combined_df = combined_df.merge(
-    br_info, how="left", left_on="customer_state", right_on="sigla"
-)
+main_df = main_df.merge(br_info, how="left", left_on="customer_state", right_on="sigla")
 
-combined_df = combined_df.merge(
+main_df = main_df.merge(
     geo_group,
     how="left",
     left_on="customer_zip_code_prefix",
@@ -100,17 +110,16 @@ combined_df = combined_df.merge(
 valid_latitude_range = (-33.75, 5.3)
 valid_longitude_range = (-73.98, -34.79)
 
-combined_df = combined_df[
-    (combined_df["geolocation_lat"] >= valid_latitude_range[0])
-    & (combined_df["geolocation_lat"] <= valid_latitude_range[1])
-    & (combined_df["geolocation_lng"] >= valid_longitude_range[0])
-    & (combined_df["geolocation_lng"] <= valid_longitude_range[1])
+main_df = main_df[
+    (main_df["geolocation_lat"] >= valid_latitude_range[0])
+    & (main_df["geolocation_lat"] <= valid_latitude_range[1])
+    & (main_df["geolocation_lng"] >= valid_longitude_range[0])
+    & (main_df["geolocation_lng"] <= valid_longitude_range[1])
 ]
 
-combined_2017_df = combined_df[combined_df["year"] == 2017]
-combined_df.drop_duplicates(inplace=True)
-lats = list(combined_2017_df["geolocation_lat"].dropna().values)
-longs = list(combined_2017_df["geolocation_lng"].dropna().values)
+main_df.drop_duplicates(inplace=True)
+lats = list(main_df["geolocation_lat"].dropna().values)
+longs = list(main_df["geolocation_lng"].dropna().values)
 locations = list(zip(lats, longs))
 map1 = folium.Map(location=[-15, -50], zoom_start=4.0)
 FastMarkerCluster(data=locations).add_to(map1)
